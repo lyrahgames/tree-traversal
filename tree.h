201a78b6807576a7b64946564a61e44ad3fe3c3c
lyrahgames <markuspawellek@gmail.com>
2019-03-07 18:41:28 +0100

    Implement traversor as generalization of iterators

--- tree.h
+++ tree.h
@@ -15,13 +15,46 @@ class node {
   std::unique_ptr<node<T>> right_{};
 };
 
-template <typename T>
-class iterator {
-  iterator() = delete;
-  iterator(node<T>* pointer) : pointer_{pointer} {};
+template <typename T, bool Constant = true>
+class traversor {
+ public:
+  using node_type = node<T>;
+  using value_type = T;
+  using difference_type = long;
+  using reference =
+      std::conditional_t<Constant, const value_type&, value_type&>;
+  using pointer = std::conditional_t<Constant, const value_type*, value_type*>;
+  using node_pointer =
+      std::conditional_t<Constant, const node_type*, node_type*>;
+
+  traversor() = delete;  // There should be no default constructor.
+  traversor(node_pointer n) : node_{n} {}
+  traversor(const traversor& it) = default;
+  traversor& operator=(const traversor& it) = default;
+  traversor(traversor&& it) = default;
+  traversor& operator=(traversor&& it) = default;
+  ~traversor() = default;
+
+  traversor& traverse_left() {
+    node_ = node_->left_.get();
+    return *this;
+  }
+  traversor left() { return node_->left_.get(); }
+  traversor& traverse_right() {
+    node_ = node_->right_.get();
+    return *this;
+  }
+  traversor right() { return node_->right_.get(); }
+  reference operator*() const { return node_->data_; }
+  pointer operator->() const { return &node_->data; }
+  bool operator==(traversor it) const { return node_ == it.node_; }
+  bool operator!=(traversor it) const { return !(*this == it); }
+  bool is_leaf() const { return node_->left_ && node_->right_; }
+  bool has_child() const { return node_->left || node_->right_; }
+  operator bool() const { return node_ != nullptr; }
 
  private:
-  node<T>* pointer_;
+  node_pointer node_;
 };
 
 template <typename Node, typename RNG, typename Distribution>
--- tree_print.cc
+++ tree_print.cc
@@ -95,6 +95,51 @@ std::wostream& operator<<(std::wostream& os,
   return os << buffer;
 }
 
+template <typename Traversor>
+info print(wconsole_display_buffer& buffer, Traversor root, int depth,
+           int offset) {
+  if (!root) return {offset, offset};
+
+  const auto left_child = print(buffer, root.left(), depth + 1, offset);
+  offset = left_child.end;
+  const auto label = std::to_wstring(*root);
+  const auto position = offset + size(label) / 2;
+  buffer.emplace(2 * depth, offset, label);
+  offset += std::size(label);
+  const auto right_child = print(buffer, root.right(), depth + 1, offset);
+
+  // printing edges
+  if (root.left()) {
+    buffer  //
+        .emplace(2 * depth + 1, left_child.position, L'\x250C')
+        .emplace(2 * depth + 1, left_child.position + 1,
+                 std::wstring(position - left_child.position - 1, L'\x2500'));
+  }
+
+  if (root.left() && root.right())
+    buffer.emplace(2 * depth + 1, position, L'\x2534');
+  else if (root.left())
+    buffer.emplace(2 * depth + 1, position, L'\x2518');
+  else if (root.right())
+    buffer.emplace(2 * depth + 1, position, L'\x2514');
+
+  if (root.right()) {
+    buffer  //
+        .emplace(2 * depth + 1, position + 1,
+                 std::wstring(right_child.position - position - 1, L'\x2500'))
+        .emplace(2 * depth + 1, right_child.position, L'\x2510');
+  }
+
+  return {position, right_child.end};
+}
+
+template <typename Traversor>
+std::wostream& operator<<(std::wostream& os, Traversor root) {
+  wconsole_display_buffer buffer{};
+  const auto tmp = print(buffer, root, 0, 0);
+  return os << buffer;
+}
+
 int main(int argc, char** argv) {
   std::setlocale(LC_ALL, "");
 
@@ -107,5 +152,5 @@ int main(int argc, char** argv) {
                     std::uniform_real_distribution<float>>
       rtm{std::uniform_real_distribution<float>{-100, 100}};
   auto root = rtm(n);
-  std::wcout << root;
+  std::wcout << traversor<float>{root.get()};
 }
